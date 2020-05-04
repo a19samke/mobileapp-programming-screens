2a548e8e2114cff9391412b9b8fa3e68cd292fc2
samuel <a19samke@student.his.se>
2020-05-04 16:20:57 +0200

    oppen second activity java

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -10,22 +10,19 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button button = findViewById(R.id.start_second_activity);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                intent.putExtra("Name", "Samuel");
                 startActivity(intent);
             }
-
         });
-
-
-
-
     }
+
 }
